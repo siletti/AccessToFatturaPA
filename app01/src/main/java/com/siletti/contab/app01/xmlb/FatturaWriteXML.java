7b5077d9ee97d10ae891b440941d96b52118ab9e
Alberto Siletti <alberto.siletti@gmail.com>
2018-12-30 22:26:55 +0100

    temp

--- app01/src/main/java/com/siletti/contab/app01/xmlb/FatturaWriteXML.java
+++ app01/src/main/java/com/siletti/contab/app01/xmlb/FatturaWriteXML.java
@@ -113,7 +113,7 @@ public class FatturaWriteXML {
 				} else if (!pecDestinatario.isEmpty()){
 					myDatiTrasmissioneType.setPECDestinatario(pecDestinatario);
 				} else {
-					return "Manca Codice e Pec Destinatario";
+					return "FAT."+ clienteNFattura +" Manca Codice e Pec Destinatario";
 				}
 				myDatiTrasmissioneType.addNewContattiTrasmittente().setTelefono("015666253");
 				myDatiTrasmissioneType.getContattiTrasmittente().setEmail("info@siletti.it");
@@ -204,13 +204,16 @@ public class FatturaWriteXML {
 						BigDecimal iva = new BigDecimal(row3.getString("DescIva"));
 						dettaglioLinee.setAliquotaIVA(iva);
 						if (iva.compareTo(BigDecimal.ZERO) == 0) {
-							if(row3.getString("CodiceIva").equals("41")) {
-		    						dettaglioLinee.setNatura(NaturaType.N_3);
-							} else 
-							if(row3.getString("CodiceIva").equals("8")) {
-								dettaglioLinee.setNatura(NaturaType.N_2);
-		    					} 
-							else return "FAT."+ clienteNFattura + " Cod.Iva non riconosciuto";
+							switch (row3.getString("CodiceIva")) {
+								case "41":
+									dettaglioLinee.setNatura(NaturaType.N_3);
+									break;
+								case "8":
+									dettaglioLinee.setNatura(NaturaType.N_2);
+									break;
+								default:
+									return "FAT."+ clienteNFattura + " Cod.Iva non riconosciuto";
+							}
 						}
 					}
 				}
@@ -229,13 +232,18 @@ public class FatturaWriteXML {
 			    			datiR.setImposta(new BigDecimal(map.get("rImposta")));
 			    			if ( aliquota.compareTo(BigDecimal.ZERO) == 0 ) {
 			    				datiR.setRiferimentoNormativo(map.get("rDescrizione"));
-			    				if(map.get("rCodiceIva").equals("41")) {
-			    					datiR.setNatura(NaturaType.N_3);
-			    				} else 
-			    				if(map.get("rCodiceIva").equals("8")) {
-			    					datiR.setNatura(NaturaType.N_2);
-			    				} else return "FAT."+ clienteNFattura + " campo Natura? Cod.Iva: "+map.get("rCodiceIva")+" non riconosciuto";
-			    			} else datiR.setEsigibilitaIVA(EsigibilitaIVAType.I);
+			    				System.out.println("c.iva: "+ map.get("rCodiceIva") +" fat."+clienteNFattura );
+			    				switch (map.get("rCodiceIva")) {
+								case "41":
+									datiR.setNatura(NaturaType.N_3);
+									break;
+								case "8":
+									datiR.setNatura(NaturaType.N_2);
+									break;
+								default:
+									return "FAT."+ clienteNFattura + " campo Natura? Cod.Iva: "+map.get("rCodiceIva")+" non riconosciuto";
+							}
+			    			}
 			    		}
 			    }
 				
